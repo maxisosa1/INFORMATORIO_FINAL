26a5ffe26eb3d738625c5a772ae72fd05ccd957a
Maximiliano Sosa <maximora470@gmail.com>
2023-12-11 17:10:36 -0300

    agregado class Meta y def delete en models de noticias

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -1,5 +1,7 @@
+from typing import Any
 from django.db import models
 from apps.usuarios.models import Usuario
+
 
 class Categoria(models.Model):
 	nombre = models.CharField(max_length = 60)
@@ -9,14 +11,21 @@ class Categoria(models.Model):
 
 class Noticia(models.Model):
 
-	titulo = models.CharField(max_length = 150)
-	cuerpo = models.TextField()
+	titulo = models.CharField(max_length = 150, null= False)
+	cuerpo = models.TextField(null = False)
 	imagen = models.ImageField(upload_to = 'noticias')
 	categoria_noticia = models.ForeignKey(Categoria, on_delete = models.CASCADE)
 	fecha = models.DateTimeField(auto_now_add=True)
 
+	class Meta:
+		ordering = ('-fecha',)
+	
 	def __str__(self):
 		return self.titulo
+	
+	def delete(self, using = None, keep_parents = False):
+		self.imagen.delete(self.imagen.name)
+		super().delete()
 
 class Comentario(models.Model):
 	usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
