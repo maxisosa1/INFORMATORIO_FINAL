886b3558b7007b7444ed0156cbad57f694a367ab
Maximiliano Sosa <maximora470@gmail.com>
2023-12-29 01:24:13 -0300

    solo falta roles y filtrar por fecha

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from apps.noticias.models import Noticia
 
 #request 'es un diccionario que continuamente se va pasando entre el navegador y el servidor'
 
 def Home(request):
 
-	return render(request, 't_home.html')
+	noticias = Noticia.objects.all().order_by("fecha")[:3]
+
+	return render(request, 't_home.html', {"noticias" : noticias})
 
 
 def Nosotros(request):
